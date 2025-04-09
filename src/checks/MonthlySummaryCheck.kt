d97d7f6083f0b3080310d9e329c4c33e86b40193
Mustafa Ibrahim <118114457+MustafaIbrahim96@users.noreply.github.com>
2025-04-09 23:20:25 +0200

    add checks for monthly summary

---
+++ src/checks/MonthlySummaryCheck.kt
@@ -0,0 +1,125 @@
+package checks
+
+import com.sun.tools.javac.Main
+import data_source.DefaultDataSource
+import domain.model.Category
+import model.MonthlySummary
+import model.Transaction
+import model.TransactionType
+import repository.TransactionRepository
+import java.time.LocalDate
+import java.util.*
+
+fun main() {
+
+    val someDemoTransaction = listOf(
+        Transaction(
+            amount = 20000.0,
+            type = TransactionType.INCOME,
+            category = Category.SALARY,
+            date = LocalDate.of(2025, 1, 1)
+        ),
+        Transaction(
+            amount = 1000.0,
+            type = TransactionType.INCOME,
+            category = Category.OTHER,
+            date = LocalDate.of(2025, 1, 4)
+        ),
+        Transaction(
+            amount = 8000.0,
+            type = TransactionType.EXPENSES,
+            category = Category.RENT,
+            date = LocalDate.of(2025, 1, 5)
+        ),
+        Transaction(
+            amount = 500.0,
+            type = TransactionType.EXPENSES,
+            category = Category.TRANSPORT,
+            date = LocalDate.of(2025, 1, 20)
+        ),
+        Transaction(
+            amount = 1000.0,
+            type = TransactionType.INCOME,
+            category = Category.OTHER,
+            date = LocalDate.of(2025, 1, 31)
+        )
+    )
+    val dataSource = object : DefaultDataSource {
+        override fun addTransactions(transaction: Transaction): Transaction {
+            TODO("Not yet implemented")
+        }
+
+        override fun deleteTransaction(id: UUID): Boolean {
+            TODO("Not yet implemented")
+        }
+
+        override fun getAllTransactions(): List<Transaction> = someDemoTransaction
+
+        override fun getTransactionById(id: UUID): Transaction {
+            TODO("Not yet implemented")
+        }
+
+        override fun updateTransaction(transaction: Transaction): Transaction {
+            TODO("Not yet implemented")
+        }
+
+    }
+    val repository = TransactionRepository(dataSource)
+    check(
+        "When Valid Month It Should return MonthlySummary successfully",
+        repository.getMonthlySummaryReport(1),
+        MonthlySummary(
+            1,
+            2025,
+            22_000.0,
+            8_500.0,
+            13_500.0
+        )
+    )
+    check(
+        "When there are no transactions in the specified month, it should return null",
+        repository.getMonthlySummaryReport(2),
+        null
+    )
+    check(
+        "When the user enters a negative month number, it should return null",
+        repository.getMonthlySummaryReport(-1),
+        null
+    )
+    check(
+        "When the user enters a month less than 1, it should return null",
+        repository.getMonthlySummaryReport(0),
+        null
+    )
+    check(
+        "When the user enters a month greater than 12, it should return null",
+        repository.getMonthlySummaryReport(13),
+        null
+    )
+    check(
+        "When the user enters a negative year, it should return null",
+        repository.getMonthlySummaryReport(1, -1),
+        null
+    )
+    check(
+        "When the specified year has no transactions but the month is valid, it should return null",
+        repository.getMonthlySummaryReport(1, 2000),
+        null
+    )
+    check(
+        "When the user enters a future year, it should return null",
+        repository.getMonthlySummaryReport(1, 2030),
+        null
+    )
+
+}
+
+fun <T> check(
+    message: String,
+    result: T,
+    correctResult: T
+) {
+    println(message)
+    if (result == correctResult) println("successful") else println("fiald")
+    println("--------")
+}
