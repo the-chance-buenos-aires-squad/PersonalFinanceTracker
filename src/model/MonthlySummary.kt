7849a2ed17e3fd295ac200e5753b95f34d008678
Mustafa Ibrahim <118114457+MustafaIbrahim96@users.noreply.github.com>
2025-04-09 23:22:09 +0200

    add get monthly summary function and monthly summary model

--- src/model/MonthlySummary.kt
+++ src/model/MonthlySummary.kt
@@ -5,7 +5,6 @@ import domain.model.Category
 data class MonthlySummary(
     val month : Int,
     val year : Int,
-    //val categories : List<Category>,
     val totalIncome : Double,
     val totalExpense : Double,
     val balance : Double,
