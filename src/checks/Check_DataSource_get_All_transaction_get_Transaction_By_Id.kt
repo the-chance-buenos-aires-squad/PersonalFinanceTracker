4c3e2f355db08386572f159109eacb258c647cc7
Farah-Dev4 <farah.maytham.ahmed@gmail.com>
2025-04-09 19:30:11 +0300

    test: add test causes for get all and get by id transactions

--- src/checks/Check_DataSource_get_All_transaction_get_Transaction_By_Id.kt
+++ src/checks/Check_DataSource_get_All_transaction_get_Transaction_By_Id.kt
@@ -1,5 +1,6 @@
 package checks
 
+import domain.model.Category
 import model.Transaction
 import model.TransactionType
 import java.time.LocalDate
@@ -10,39 +11,38 @@ fun main() {
     checkGetByIdReturnsNullIfNotFound()
     checkGetAllReturnsCorrectTransaction()
     checkGetAllreturnsEmptyListIfNoTransaction()
-
-
 }
-// لما ابحث عن ID موجود يرجعلي Transaction صح
+
 fun checkGetByIdReturnsCorrectTransaction() {
    val transactionList = mutableListOf<Transaction>()
-    val tx = Transaction( id = 1,amount = 100.0, category = "Food", type = TransactionType.EXPENSE, date = LocalDate.now())
+    val id = UUID.randomUUID()
+    val tx = Transaction( id = id,amount = 100.0, category = Category.FOOD, type = TransactionType.EXPENSE, date = LocalDate.now())
     transactionList.add(tx)
-    val result = transactionList.find { it.id == 1}
-    check ("Should return transaction with ID 1",result?.id==1,true)
+    val result = transactionList.find { it.id == id}
+    check ("Should return transaction with the same id",result?.id==id,true)
  }
-//لما ابحث عن ID غير موجود يطلعلي Null
+
 fun  checkGetByIdReturnsNullIfNotFound() {
     val transactionList = mutableListOf<Transaction>()
-    val tx = Transaction( id = UUID.randomUUID(),amount = 100.0,category = "Food", type = TransactionType.EXPENSE,   date = LocalDate.now())
+    val tx = Transaction( id = UUID.randomUUID(),amount = 100.0,category = Category.FOOD, type = TransactionType.EXPENSE,   date = LocalDate.now())
     transactionList.add(tx)
     val result = transactionList.find {it.id.toString()=="99"}
     check("Should return null if transaction not found", result==null,true)
 }
-// يرجع كل المعاملات المضافة
+
 fun checkGetAllReturnsCorrectTransaction() {
     val transactionList = mutableListOf<Transaction>()
-    val tx1 = Transaction(id= 80,amount = 100.0,category = "Food", type = TransactionType.EXPENSE,date = LocalDate.now())
-    val tx2 = Transaction(id = 90, amount = 200.0, category = "Food", type = TransactionType.INCOME,date = LocalDate.now())
+    val tx1 = Transaction(id= UUID.randomUUID(),amount = 100.0,category = Category.FOOD, type = TransactionType.EXPENSE,date = LocalDate.now())
+    val tx2 = Transaction(id = UUID.randomUUID(), amount = 200.0, category = Category.FOOD, type = TransactionType.INCOME,date = LocalDate.now())
     transactionList.add(tx1)
     transactionList.add(tx2)
-    val result = transactionList.getAll()
+    val result = transactionList
     check( "Should return list of size 2",result.size ==2,true)
 }
-// اذا ماكو معاملات يرجع كل القيم فارغة
+
 fun checkGetAllreturnsEmptyListIfNoTransaction() {
     val transactionList = mutableListOf<Transaction>()
-    val result = transactionList.getAll()
-    check( "Should return empty list when no tansaction", transactionList.isEmpty(),true)
+    val result = transactionList
+    check( "Should return empty list when no transaction", transactionList.isEmpty(),true)
 }
 
