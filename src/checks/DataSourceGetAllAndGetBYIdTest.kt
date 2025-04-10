76b7df4a8c6f8933326f3d5bd1c5665f0ae3fef1
Farah-Dev4 <farah.maytham.ahmed@gmail.com>
2025-04-10 11:49:48 +0300

    feat: implement getAllTransactions and getTransactionById, and rename test file

--- src/checks/DataSourceGetAllAndGetBYIdTest.kt
+++ src/checks/DataSourceGetAllAndGetBYIdTest.kt
@@ -1,5 +1,6 @@
 package checks
 
+import data_source.DataSource
 import domain.model.Category
 import model.Transaction
 import model.TransactionType
@@ -14,35 +15,34 @@ fun main() {
 }
 
 fun checkGetByIdReturnsCorrectTransaction() {
-   val transactionList = mutableListOf<Transaction>()
+   val dataSource = DataSource()
     val id = UUID.randomUUID()
     val tx = Transaction( id = id,amount = 100.0, category = Category.FOOD, type = TransactionType.EXPENSE, date = LocalDate.now())
-    transactionList.add(tx)
-    val result = transactionList.find { it.id == id}
+    dataSource.addTransactions(tx)
+    val result = dataSource.getTransactionById(id)
     check ("Should return transaction with the same id",result?.id==id,true)
  }
 
 fun  checkGetByIdReturnsNullIfNotFound() {
-    val transactionList = mutableListOf<Transaction>()
-    val tx = Transaction( id = UUID.randomUUID(),amount = 100.0,category = Category.FOOD, type = TransactionType.EXPENSE,   date = LocalDate.now())
-    transactionList.add(tx)
-    val result = transactionList.find {it.id.toString()=="99"}
+    val dataSource = DataSource()
+    val fakeId= UUID.randomUUID()
+    val result = dataSource.getTransactionById(fakeId)
     check("Should return null if transaction not found", result==null,true)
 }
 
 fun checkGetAllReturnsCorrectTransaction() {
-    val transactionList = mutableListOf<Transaction>()
+    val dataSource = DataSource()
     val tx1 = Transaction(id= UUID.randomUUID(),amount = 100.0,category = Category.FOOD, type = TransactionType.EXPENSE,date = LocalDate.now())
     val tx2 = Transaction(id = UUID.randomUUID(), amount = 200.0, category = Category.FOOD, type = TransactionType.INCOME,date = LocalDate.now())
-    transactionList.add(tx1)
-    transactionList.add(tx2)
-    val result = transactionList
+    dataSource.addTransactions(tx1)
+    dataSource.addTransactions(tx2)
+    val result = dataSource.getAllTransactions()
     check( "Should return list of size 2",result.size ==2,true)
 }
 
 fun checkGetAllreturnsEmptyListIfNoTransaction() {
-    val transactionList = mutableListOf<Transaction>()
-    val result = transactionList
-    check( "Should return empty list when no transaction", transactionList.isEmpty(),true)
+    val dataSource = DataSource()
+    val result = dataSource.getAllTransactions()
+    check( "Should return empty list when no transaction", result.isEmpty(),true)
 }
 
