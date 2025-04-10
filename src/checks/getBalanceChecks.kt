803ca65b1bcdc4fd33e466864f9c64b09eba2fa3
AhmedMohamed056 <hamadarayyan056@gmail.com>
2025-04-10 20:59:52 +0200

    add new test cases

--- src/checks/getBalanceChecks.kt
+++ src/checks/getBalanceChecks.kt
@@ -1,20 +1,59 @@
 package checks
 
 import data_source.InMemoryTransactionsDataSource
+import model.Transaction
+import model.TransactionCategory
+import model.TransactionType
 import repository.TransactionManager
+import java.time.LocalDate
+import java.util.*
 
 
 fun runBalanceChecks() {
+    val transactions = listOf(
+        Transaction(UUID.randomUUID(), 1000.0, TransactionCategory.SALARY, TransactionType.INCOME, LocalDate.now()),
+        Transaction(UUID.randomUUID(), 200.0, TransactionCategory.SALARY, TransactionType.EXPENSES, LocalDate.now()),
+        Transaction(UUID.randomUUID(), 500.0, TransactionCategory.SALARY, TransactionType.INCOME, LocalDate.now()),
+        Transaction(UUID.randomUUID(), 100.0, TransactionCategory.SALARY, TransactionType.EXPENSES, LocalDate.now())
+    )
+
     val dataSource = InMemoryTransactionsDataSource()
-    val transactionManager = TransactionManager(dataSource).getBalance()
+    val transactionManager = TransactionManager(dataSource)
+    for (data in transactions) {
+        dataSource.addTransactions(data)
+    }
     check(
-        message = "Balance Greater Than Zero",
-        result = transactionManager >= 0,
+        message = "when Income greater than expense",
+        result = transactionManager.getBalance() == 1200.0,
         correctResult = true
     )
+
+    dataSource.addTransactions(
+        Transaction(
+            UUID.randomUUID(),
+            1200.0,
+            TransactionCategory.SALARY,
+            TransactionType.EXPENSES,
+            LocalDate.now()
+        )
+    )
     check(
-        message = "Invalid Balance return false",
-        result = transactionManager < 0,
+        message = "when income equal than expense it will return zero",
+        result = transactionManager.getBalance() == 0.0,
+        correctResult = true
+    )
+    dataSource.addTransactions(
+        Transaction(
+            UUID.randomUUID(),
+            200.0,
+            TransactionCategory.SALARY,
+            TransactionType.EXPENSES,
+            LocalDate.now()
+        )
+    )
+    check(
+        message = "when balance less than zero it's invalid value",
+        result = transactionManager.getBalance() < 0.0,
         correctResult = false
     )
 }
