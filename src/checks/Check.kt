76ba509e8b44879844c70151ab3536354a726c6e
Amr4x4 <amranwarx2@gmail.com>
2025-04-10 17:04:49 +0200

    finishing the testcases and implementation for { add, update and delete } in Data Source.

--- src/checks/Check.kt
+++ src/checks/Check.kt
@@ -1,7 +1,63 @@
 package checks
 
+import data_source.InMemoryTransactionsDataSource
+import model.Transaction
+import model.TransactionCategory
+import model.TransactionType
+import java.time.LocalDate
+
 fun main() {
 
+    val dataSource = InMemoryTransactionsDataSource()
+    val transaction1 = Transaction(
+        amount = 100.0,
+        transactionCategory = TransactionCategory.FOOD,
+        type = TransactionType.EXPENSES,
+        date = LocalDate.now()
+    )
+    val transaction2 = Transaction(
+        amount = 200.0,
+        transactionCategory = TransactionCategory.RENT,
+        type = TransactionType.EXPENSES,
+        date = LocalDate.now()
+    )
+
+    check(
+        message = "Add transaction should return true",
+        result = dataSource.addTransactions(transaction1),
+        correctResult = true
+    )
+
+    check(
+        message = "Add existing transaction should return false",
+        result = dataSource.addTransactions(transaction1),
+        correctResult = false
+    )
+
+    check(
+        message = "Delete transaction should return true",
+        result = dataSource.deleteTransaction(transaction1.id),
+        correctResult = true
+    )
+
+    check(
+        message = "Delete non-existing transaction should return false",
+        result = dataSource.deleteTransaction(transaction1.id),
+        correctResult = false
+    )
+
+    // Need to negotiation
+    check(
+        message = "Update transaction should return true",
+        result = dataSource.updateTransaction(transaction2),
+        correctResult = true
+    )
+
+    check(
+        message = "Update non-existing transaction should return false",
+        result = dataSource.updateTransaction(transaction1),
+        correctResult = false
+    )
 
 }
 
--- src/data_source/InMemoryTransactionsDataSource.kt
+++ src/data_source/InMemoryTransactionsDataSource.kt
@@ -5,24 +5,36 @@ import java.util.UUID
 
 class InMemoryTransactionsDataSource :TransactionDataSource{
     private val transactionList = mutableListOf<Transaction>()
-    override fun addTransactions(transaction: Transaction): Boolean {
-        TODO("Not yet implemented")
-    }
 
+    override fun addTransactions(transaction: Transaction): Boolean {
+        return if (transactionList.any { it.id == transaction.id }) {
+            false
+        } else {
+            transactionList.add(transaction)
+            true
+        }
+    }
 
     override fun deleteTransaction(id: UUID): Boolean {
-        TODO("Not yet implemented")
+        return transactionList.removeIf { it.id == id }
     }
 
+    // Need to negotiation
+    override fun updateTransaction(transaction: Transaction): Boolean {
+        val existingTransaction = transactionList.find { it.id == transaction.id }
+        return if (existingTransaction != null) {
+            val index = transactionList.indexOf(existingTransaction)
+            transactionList[index] = transaction
+            true
+        } else {
+            false
+        }
+    }
     override fun getAllTransactions(): List<Transaction> {
         TODO("Not yet implemented")
     }
 
     override fun getTransactionById(id: UUID): Transaction? {
-        TODO("Not yet implemented")
-    }
-
-    override fun updateTransaction(transaction: Transaction): Boolean {
         TODO("Not yet implemented")
     }
 
