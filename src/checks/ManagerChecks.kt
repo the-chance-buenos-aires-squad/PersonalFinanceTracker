a20e4c0a5866637f9d51d9df0ae4cce6d1539842
AhmedMohamed056 <hamadarayyan056@gmail.com>
2025-04-12 13:03:20 +0200

    refactor checks file

---
+++ src/checks/ManagerChecks.kt
@@ -0,0 +1,345 @@
+package checks
+
+import data_source.InMemoryTransactionsDataSource
+import model.*
+import repository.TransactionManager
+import java.time.LocalDate
+import java.util.*
+
+fun main() {
+    runBalanceChecks()
+    runMonthlySummaryChecks()
+    runGetAllGetByIdChecks()
+    //runAddDeleteUpdateTransactionChecks()
+}
+
+fun runBalanceChecks() {
+    val dataSource = InMemoryTransactionsDataSource()
+    val transactionManager = TransactionManager(dataSource)
+    val transactions = listOf(
+        Transaction(UUID.randomUUID(), 1000.0, TransactionCategory.SALARY, TransactionType.INCOME, LocalDate.now()),
+        Transaction(
+            UUID.randomUUID(),
+            200.0,
+            TransactionCategory.SALARY,
+            TransactionType.EXPENSES,
+            LocalDate.now()
+        ),
+        Transaction(UUID.randomUUID(), 500.0, TransactionCategory.SALARY, TransactionType.INCOME, LocalDate.now()),
+        Transaction(UUID.randomUUID(), 100.0, TransactionCategory.SALARY, TransactionType.EXPENSES, LocalDate.now())
+    )
+    transactions.forEach {
+        dataSource.addTransactions(it)
+    }
+    check(
+        message = "when Income greater than expense should return positive value",
+        result = transactionManager.getBalance() == 1200.0,
+        correctResult = true
+    )
+
+    dataSource.addTransactions(
+        Transaction(
+            UUID.randomUUID(),
+            1200.0,
+            TransactionCategory.SALARY,
+            TransactionType.EXPENSES,
+            LocalDate.now()
+        )
+    )
+    check(
+        message = "when income equal than expense it will return zero",
+        result = transactionManager.getBalance() == 0.0,
+        correctResult = true
+    )
+    dataSource.addTransactions(
+        Transaction(
+            UUID.randomUUID(),
+            200.0,
+            TransactionCategory.SALARY,
+            TransactionType.EXPENSES,
+            LocalDate.now()
+        )
+    )
+    check(
+        message = "when balance less than zero it's invalid value",
+        result = transactionManager.getBalance() < 0.0,
+        correctResult = false
+    )
+}
+
+fun runMonthlySummaryChecks() {
+    val dataSource = InMemoryTransactionsDataSource()
+    val transactionManager = TransactionManager(dataSource)
+    dataSource.addTransactions(
+        Transaction(
+            amount = 20000.0,
+            type = TransactionType.INCOME,
+            transactionCategory = TransactionCategory.SALARY,
+            date = LocalDate.of(2025, 1, 1)
+        )
+    )
+    dataSource.addTransactions(
+        Transaction(
+            amount = 1000.0,
+            type = TransactionType.INCOME,
+            transactionCategory = TransactionCategory.OTHER,
+            date = LocalDate.of(2025, 1, 5)
+        )
+    )
+    dataSource.addTransactions(
+        Transaction(
+            amount = 1000.0,
+            type = TransactionType.INCOME,
+            transactionCategory = TransactionCategory.OTHER,
+            date = LocalDate.of(2025, 1, 10)
+        )
+    )
+    dataSource.addTransactions(
+        Transaction(
+            amount = 8000.0,
+            type = TransactionType.EXPENSES,
+            transactionCategory = TransactionCategory.RENT,
+            date = LocalDate.of(2025, 1, 14)
+        )
+    )
+    dataSource.addTransactions(
+        Transaction(
+            amount = 500.0,
+            type = TransactionType.EXPENSES,
+            transactionCategory = TransactionCategory.TRANSPORT,
+            date = LocalDate.of(2025, 1, 20)
+        )
+    )
+    dataSource.addTransactions(
+        Transaction(
+            amount = 20000.0,
+            type = TransactionType.INCOME,
+            transactionCategory = TransactionCategory.SALARY,
+            date = LocalDate.of(2025, 3, 1)
+        )
+    )
+    dataSource.addTransactions(
+        Transaction(
+            amount = 1000.0,
+            type = TransactionType.INCOME,
+            transactionCategory = TransactionCategory.OTHER,
+            date = LocalDate.of(2025, 3, 5)
+        )
+    )
+    dataSource.addTransactions(
+        Transaction(
+            amount = 1000.0,
+            type = TransactionType.INCOME,
+            transactionCategory = TransactionCategory.OTHER,
+            date = LocalDate.of(2025, 3, 10)
+        )
+    )
+    dataSource.addTransactions(
+        Transaction(
+            amount = 8000.0,
+            type = TransactionType.EXPENSES,
+            transactionCategory = TransactionCategory.RENT,
+            date = LocalDate.of(2025, 5, 14)
+        )
+    )
+    dataSource.addTransactions(
+        Transaction(
+            amount = 500.0,
+            type = TransactionType.EXPENSES,
+            transactionCategory = TransactionCategory.TRANSPORT,
+            date = LocalDate.of(2025, 5, 20)
+        )
+    )
+    val memoryTransactionsDataSource = dataSource.getAllTransactions()
+    check(
+        "When Valid Month It Should return MonthlySummary successfully",
+        transactionManager.getMonthlySummaryReport(1) == MonthlySummary(
+            totalIncome = 22000.0,
+            totalExpense = 8500.0,
+            incomeList = memoryTransactionsDataSource.filter {
+                it.type == TransactionType.INCOME && it.date.monthValue == 1 && it.date.year == LocalDate.now().year
+            },
+            expenseList = memoryTransactionsDataSource.filter {
+                it.type == TransactionType.EXPENSES && it.date.monthValue == 1 && it.date.year == LocalDate.now().year
+            },
+            highestIncomeCategory = TopCategory(20000.0, TransactionCategory.SALARY),
+            highestExpenseCategory = TopCategory(8000.0, TransactionCategory.RENT)
+        ),
+        true
+
+    )
+    check(
+        "When Valid Month but no expense" + "It Should return MonthlySummary empty expenseList and highestExpendCategory null",
+        transactionManager.getMonthlySummaryReport(3) == MonthlySummary(
+            totalIncome = 22000.0,
+            totalExpense = 0.0,
+            incomeList = memoryTransactionsDataSource.filter {
+                it.type == TransactionType.INCOME && it.date.monthValue == 3 && it.date.year == LocalDate.now().year
+            },
+            expenseList = listOf(),
+            highestIncomeCategory = TopCategory(20000.0, TransactionCategory.SALARY),
+            highestExpenseCategory = null
+        ),
+        true
+
+    )
+    check(
+        "When Valid Month but no Income" + "It Should return MonthlySummary empty incomeList and highestIncomeCategory null",
+        transactionManager.getMonthlySummaryReport(5) == MonthlySummary(
+            totalIncome = 0.0,
+            totalExpense = 8500.0,
+            incomeList = listOf(),
+            expenseList = memoryTransactionsDataSource.filter {
+                it.type == TransactionType.EXPENSES && it.date.monthValue == 5
+                        && it.date.year == LocalDate.now().year
+            },
+            highestIncomeCategory = null,
+            highestExpenseCategory = TopCategory(8000.0, TransactionCategory.RENT)
+        ),
+        true
+
+    )
+    check(
+        "When there are no transactions in the specified month, it should return null",
+        transactionManager.getMonthlySummaryReport(2) == null,
+        true
+    )
+    check(
+        "When the user enters a negative month number, it should return null",
+        transactionManager.getMonthlySummaryReport(-1) == null,
+        true
+    )
+    check(
+        "When the user enters a month less than 1, it should return null",
+        transactionManager.getMonthlySummaryReport(0) == null,
+        true
+    )
+    check(
+        "When the user enters a month greater than 12, it should return null",
+        transactionManager.getMonthlySummaryReport(13) == null,
+        true
+    )
+    check(
+        "When the user enters a negative year, it should return null",
+        transactionManager.getMonthlySummaryReport(1, -1) == null,
+        true
+    )
+    check(
+        "When the specified year has no transactions but the month is valid, it should return null",
+        transactionManager.getMonthlySummaryReport(1, 2000) == null,
+        true
+    )
+    check(
+        "When the user enters a future year, it should return null",
+        transactionManager.getMonthlySummaryReport(1, 2030) == null,
+        true
+    )
+}
+
+fun runGetAllGetByIdChecks() {
+    val dataSource = InMemoryTransactionsDataSource()
+    val transactionManager = TransactionManager(dataSource)
+    dataSource.addTransactions(
+        Transaction(
+            amount = 20000.0,
+            type = TransactionType.EXPENSES,
+            transactionCategory = TransactionCategory.SALARY,
+            date = LocalDate.of(2025, 1, 1)
+        )
+    )
+    dataSource.addTransactions(
+        Transaction(
+            amount = 1000.0,
+            type = TransactionType.EXPENSES,
+            transactionCategory = TransactionCategory.OTHER,
+            date = LocalDate.of(2025, 1, 5)
+        )
+    )
+    dataSource.addTransactions(
+        Transaction(
+            amount = 1000.0,
+            type = TransactionType.EXPENSES,
+            transactionCategory = TransactionCategory.OTHER,
+            date = LocalDate.of(2025, 1, 10)
+        )
+    )
+    dataSource.addTransactions(
+        Transaction(
+            amount = 8000.0,
+            type = TransactionType.EXPENSES,
+            transactionCategory = TransactionCategory.RENT,
+            date = LocalDate.of(2025, 1, 14)
+        )
+    )
+    dataSource.addTransactions(
+        Transaction(
+            amount = 500.0,
+            type = TransactionType.EXPENSES,
+            transactionCategory = TransactionCategory.TRANSPORT,
+            date = LocalDate.of(2025, 1, 20)
+        )
+    )
+
+
+
+
+    check(
+        message = "when The number of transactions is 5 should return true",
+        result = transactionManager.getAllTransactions().size == 5,
+        correctResult = true
+    )
+
+
+    val emptyDataSource = InMemoryTransactionsDataSource()
+    val secondTransaction = TransactionManager(emptyDataSource)
+    check(
+        message = "when transactions is empty should return true",
+        result = secondTransaction.getAllTransactions().isEmpty(),
+        correctResult = true
+    )
+
+
+    val retrievedTransaction = transactionManager.getTransactionById(UUID.randomUUID())
+
+    check(
+        message = "when Retrieving by invalid ID should return null",
+        result = retrievedTransaction == null,
+        correctResult = true
+    )
+}
+
+fun runAddDeleteUpdateTransactionChecks() {
+    val dataSource = InMemoryTransactionsDataSource()
+    val transactionManager = TransactionManager(dataSource)
+    val index = 0
+    val originalTransaction = transactionManager.getAllTransactions()[index].copy() // snapshot before change
+    val updatedTransaction = originalTransaction.copy(amount = 200.0)
+    val result = transactionManager.updateTransaction(index, updatedTransaction)
+    val afterUpdateTransaction = transactionManager.getAllTransactions()[index]
+    val transaction = Transaction(
+        amount = 100.0,
+        transactionCategory = TransactionCategory.FOOD,
+        type = TransactionType.EXPENSES,
+        date = LocalDate.now()
+    )
+    check(
+        " When Add transaction to system should return true",
+        result = transactionManager.addTransaction(transaction),
+        correctResult = true
+    )
+    check(
+        "when update transaction it should return true",
+        result = result,
+        correctResult = true
+    )
+    check(
+        "when update transaction should be changed",
+        result = originalTransaction != afterUpdateTransaction,
+        correctResult = true
+    )
+    check(
+        "When delete transaction to system should return true",
+        result = transactionManager.deleteTransaction(transaction.id),
+        correctResult = true
+    )
+}
