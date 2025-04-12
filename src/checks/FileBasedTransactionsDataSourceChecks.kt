2362714a4accbf8e5a956973a5de295b98f00c82
Mostafa Gad <m.gad192@gmail.com>
2025-04-12 20:04:34 +0200

    update FileBasedTransactionsDataSourceChecks.

--- src/checks/FileBasedTransactionsDataSourceChecks.kt
+++ src/checks/FileBasedTransactionsDataSourceChecks.kt
@@ -5,15 +5,19 @@ import data_source.TransactionDataSource
 import model.Transaction
 import model.TransactionCategory
 import model.TransactionType
-import util.TransactionsFile
+import util.TransactionsFileManager
 import java.time.LocalDate
+import java.util.*
 
 fun main() {
-    val transactionsFile = TransactionsFile()
-    val dataSource = FileBasedTransactionsDataSource(transactionsFile)
+    val transactionsFileManager = TransactionsFileManager()
+    val dataSource = FileBasedTransactionsDataSource(transactionsFileManager)
 
     addTransactionsChecks(dataSource)
-
+    deleteTransactionChecks(dataSource)
+    getAllTransactionsChecks(dataSource)
+    getTransactionByIdChecks(dataSource)
+    updateTransactionChecks(dataSource)
 }
 
 fun addTransactionsChecks(dataSource: TransactionDataSource) {
@@ -27,6 +31,88 @@ fun addTransactionsChecks(dataSource: TransactionDataSource) {
                 date = LocalDate.of(2025, 1, 1)
             )
         ),
+        correctResult = true
+    )
+}
+
+fun getAllTransactionsChecks(dataSource: TransactionDataSource) {
+    check(
+        "When file doesn't exist it should return true",
+        result = dataSource.getAllTransactions().isEmpty(),
+        correctResult = true
+    )
+    check(
+        "When file exist and contains no transactions it should return true",
+        result = dataSource.getAllTransactions().isEmpty(),
+        correctResult = true
+    )
+    check(
+        "When file exist and contains transactions it should return true",
+        result = dataSource.getAllTransactions().isNotEmpty(),
+        correctResult = true
+    )
+}
+
+fun deleteTransactionChecks(dataSource: TransactionDataSource) {
+    val transactionId = UUID.randomUUID()
+    check(
+        "When file doesn't exist it should return false",
+        result = dataSource.deleteTransaction(id = transactionId),
+        correctResult = false
+    )
+    check(
+        "When file exist and contains no transaction with this id it should return false",
+        result = dataSource.deleteTransaction(id = transactionId),
+        correctResult = false
+    )
+    check(
+        "When file exist and contains transaction with this id it should return true",
+        result = dataSource.deleteTransaction(id = transactionId),
+        correctResult = true
+    )
+}
+
+fun getTransactionByIdChecks(dataSource: TransactionDataSource) {
+    val transactionId = UUID.randomUUID()
+    check(
+        "When file doesn't exist it should return false",
+        result = dataSource.getTransactionById(id = transactionId) == null,
+        correctResult = true
+    )
+    check(
+        "When file exist and contains no transaction with this id it should return true",
+        result = dataSource.getTransactionById(id = transactionId) == null,
+        correctResult = true
+    )
+    check(
+        "When file exist and contains transaction with this id it should return true",
+        result = dataSource.getTransactionById(id = transactionId) != null,
+        correctResult = true
+    )
+}
+
+fun updateTransactionChecks(dataSource: TransactionDataSource) {
+    val transactionId = UUID.randomUUID()
+    val transaction = Transaction(
+        id = transactionId,
+        amount = 200.0,
+        transactionCategory = TransactionCategory.RENT,
+        type = TransactionType.EXPENSES,
+        date = LocalDate.now()
+    )
+    check(
+        "When file doesn't exist it should return false",
+        result = dataSource.updateTransaction(transaction),
+        correctResult = false
+    )
+    check(
+        "When file exist and contains no transaction with this id it should return true",
+        result = dataSource.updateTransaction(transaction),
+        correctResult = false
+    )
+    check(
+        "When file exist and contains transaction with this id it should return true",
+        result = dataSource.updateTransaction(transaction),
         correctResult = true
     )
 }
